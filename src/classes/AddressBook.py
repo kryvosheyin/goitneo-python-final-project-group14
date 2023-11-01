407ec7d6095a65e0b5f16771a5b5cbd8c5d4ab1b
Sergii <kryvkosergii@gmail.com>
2023-11-01 13:56:33 +0200

    search in contacts by criteria

--- src/classes/AddressBook.py
+++ src/classes/AddressBook.py
@@ -1,7 +1,8 @@
-from collections import UserDict, defaultdict
-from datetime import date, datetime
+from collections import UserDict
 from classes.Name import Name
 from classes.Record import Record
+from classes.Phone import Phone
+from classes.Fields import Field
 
 
 class AddressBook(UserDict):
@@ -14,8 +15,31 @@ class AddressBook(UserDict):
             raise KeyError(f"Contact {name} is not found")
         return contact
 
+    def find_all(self, value: str) -> [Record]:
+        search_result = []
+        test_lowercase_value = self.__test_value(value.lower())
+        for record in self.data.values():
+            if any(filter(lambda val: test_lowercase_value(val), self.__scalar_variables_from_record(record))) or self.__test_phones(record.phones, test_lowercase_value):
+                search_result.append(record)
+        return search_result
+
     def delete(self, record_name: Name):
         try:
             del self.data[record_name.value]
         except KeyError:
             raise KeyError("Unable to find {record_name} in the address book")
+
+    def __test_value(self, value_in_lower: str) -> bool:
+        def test(record_value) -> bool:
+            print(
+                f"test {value_in_lower} is {record_value} = {str(record_value).lower().find(value_in_lower)}")
+            return record_value and str(record_value).lower().find(value_in_lower) > -1
+        return test
+
+    def __test_phones(self, phones: [Phone], test_lower_value) -> bool:
+        return phones and any(filter(lambda phone: test_lower_value(phone), phones))
+
+    # Returns the list of field values from object of Record type.
+    # Function represents the part of dynamic introspection to be able to search in all available fields without any additional development efforts
+    def __scalar_variables_from_record(self, record: Record) -> [Field]:
+        return list(filter(lambda value: isinstance(value, Field), record.__dict__.values()))
--- src/main.py
+++ src/main.py
@@ -156,6 +156,11 @@ def get_all(address_book: AddressBook, _):
         print(f"|{'No records found. Add at first':^133}|")
         print(f"|{'_'*133}|")
 
+def render_contacts(records:[Record], _):
+    address_book: AddressBook = AddressBook()
+    for record in records:
+        address_book.add_record(record) 
+    get_all(address_book, _)
 
 def birthdays(address_book: AddressBook, _):
     number_of_days = int(input("Please enter the number of days you want to check: "))
@@ -197,6 +202,13 @@ def save_to_file(book: AddressBook, filename: str = "address_book.pkl"):
         pickle.dump(book, file)
     return f"Address book was saved to {filename}"
 
+def find(book: AddressBook, args):
+    try:
+        search_parameter = args[0]
+    except IndexError:
+        raise IndexError("Please provide value to search")
+    records:[Record] = book.find_all(search_parameter)
+    render_contacts(records, args)
 
 def load_from_file(filename: str = "address_book.pkl") -> AddressBook:
     try:
@@ -216,6 +228,7 @@ Add new phone - 'add-phone' <name without spaces> <phone1>,<phone2>,...
 Remove phone - 'remove-phone' <name without spaces> <phone>
 Edit phone - 'edit-phone' <name without spaces> <phone-to-change> <phone-new>
 Get all phones for contact - 'get-phone' <name without spaces>
+Find contacts by value - 'find' <value containing in any field>
 Add email - 'add-email' <name without spaces> <email>
 Change email - 'change-email' <name without spaces> <email>
 Remove email - 'remove-email' <name without spaces>
@@ -245,6 +258,7 @@ def main():
         "birthdays": birthdays,
         "remove": remove,
         "all": get_all,
+        "find" : find,
     }
 
     print("Welcome to the assistant bot!")
