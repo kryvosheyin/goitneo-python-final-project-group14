0009202791dcacad47ed2f7892ef8ca2130063b6
opetry16 <everlands.op@gmail.com>
2023-11-02 21:43:58 +0100

    added address

---
+++ src/classes/address.py
@@ -0,0 +1,15 @@
+from classes.Fields import Field
+from classes.Exceptions import IncorrectAddressFormatException
+
+
+class Address(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.validate()
+
+    def validate(self):
+        if not self.value:
+            raise IncorrectAddressFormatException("Address cannot be empty")
+    
+    def __str__(self):
+        return self.value
