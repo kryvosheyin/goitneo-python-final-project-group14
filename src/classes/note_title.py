a53c419c13960c4bc3d59a5af3ddee36912c0a35
kryvosheyin <alex.kryvosheyin@gmail.com>
2023-11-03 12:35:35 +0000

    added classes

--- src/classes/note_title.py
+++ src/classes/note_title.py
@@ -1,6 +1,9 @@
 from classes.Fields import Field
+from classes.Exceptions import IncorrectTitleException
 
 
 class Title(Field):
     def __init__(self, value):
-        if 
+        if len(self.value) > 120 or type(self.value) is not str:
+            raise IncorrectTitleException("Title must not exceed 120 characters")
+        self.value = value
